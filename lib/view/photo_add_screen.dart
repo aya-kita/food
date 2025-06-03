c1fa8daf217270507cbede86f9fbea09109912cb
りんご <akari@ringonoMacBook-Air.local>
2025-06-04 04:03:40 +0900

    すいません

--- lib/view/photo_add_screen.dart
+++ lib/view/photo_add_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:food/app_router.dart';
 
 class PhotoAddScreen extends ConsumerWidget{
   const PhotoAddScreen({super.key});
---
+++ lib/view/photo_edit_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:food/app_router.dart';
+
+class  PhotoEditScreen extends ConsumerWidget{
+  const PhotoEditScreen({super.key});
+
+  @override
+  Widget build(BuildContext contect,WidgetRef ref){
+    return Scaffold(
+      
+    );
+
+  }
+}
