19122df037ba1cca31a6d0adfd4493c46c73c485
Ayaka Kitami <atari111210@gmail.com>
2025-06-08 13:52:38 +0900

    splash画面

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -5,36 +5,34 @@ import 'package:food/view/photo_list_screen.dart';
 import 'package:food/view/photo_detail_screen.dart';
 import 'package:food/view/photo_add_screen.dart';
 
-
 final router = GoRouter(
   debugLogDiagnostics: true,
   initialLocation: '/',
   routes: [
     GoRoute(
-        name: 'home',
-        path: '/',
-        builder: (context, state) => HomePage(),
-    ),
-     GoRoute(
-        name: 'add',
-        path: '/add',
-        builder: (context, state) => PhotoAddScreen(),
+      name: 'home',
+      path: '/',
+      builder: (context, state) => HomePage(),
     ),
     GoRoute(
-        name: 'list',
-        path: '/list',
-        builder: (context, state) => PhotoListScreen(),
+      name: 'add',
+      path: '/add',
+      builder: (context, state) => PhotoAddScreen(),
     ),
     GoRoute(
-        name: 'about',
-        path: '/about',
-        builder: (context, state) => PhotoDetailScreen(),
+      name: 'list',
+      path: '/list',
+      builder: (context, state) => PhotoListScreen(),
     ),
     GoRoute(
-        name: 'edit',
-        path: '/edit',
-        builder: (context, state) => PhotoEditScreen(),
+      name: 'about',
+      path: '/about',
+      builder: (context, state) => PhotoDetailScreen(),
     ),
-   
+    GoRoute(
+      name: 'edit',
+      path: '/edit',
+      builder: (context, state) => PhotoEditScreen(),
+    ),
   ],
 );
